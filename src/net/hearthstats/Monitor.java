3c62770997a5465c9c7487feb7da39aa53b0650c
Jerome Dane <jerome.dane@gmail.com>
2014-01-24 21:51:20 -0500

    Removed some unused code

--- src/net/hearthstats/Monitor.java
+++ src/net/hearthstats/Monitor.java
@@ -1,52 +1,23 @@
 package net.hearthstats;
 
-import jna.*;
-import jna.extra.GDI32Extra;
-import jna.extra.User32Extra;
-import jna.extra.WinGDIExtra;
-
-import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.awt.image.PixelGrabber;
-import java.io.File;
 import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import javax.imageio.ImageIO;
-import javax.management.Notification;
 import javax.swing.ImageIcon;
-import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import sun.java2d.pipe.PixelFillPipe;
+import jna.JnaUtilException;
 
-import com.sun.jna.Memory;
-import com.sun.jna.Native;
-import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.GDI32;
-import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef;
-import com.sun.jna.platform.win32.WinGDI;
-import com.sun.jna.platform.win32.WinDef.HBITMAP;
-import com.sun.jna.platform.win32.WinDef.HWND;
-import com.sun.jna.platform.win32.WinDef.RECT;
-import com.sun.jna.platform.win32.WinGDI.BITMAPINFO;
-import com.sun.jna.platform.win32.WinNT.HANDLE;
-import com.sun.jna.platform.win32.WinUser.WNDENUMPROC;
-
+@SuppressWarnings("serial")
 public class Monitor extends JFrame implements Observer {
 
 	protected HearthstoneAnalyzer _analyzer = new HearthstoneAnalyzer();
@@ -188,9 +159,8 @@ public class Monitor extends JFrame implements Observer {
 		}
 	}
 	
-	@SuppressWarnings("unchecked")
 	protected void _pollHearthstone() {
-		scheduledExecutorService.schedule(new Callable() {
+		scheduledExecutorService.schedule(new Callable<Object>() {
 			public Object call() throws Exception {
 				
 				if (_hsHelper.foundProgram())
