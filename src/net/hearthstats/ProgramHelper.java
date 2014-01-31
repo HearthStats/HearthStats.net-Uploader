5ad64a9df4497e71035b831a748fb3ffe8aa1915
Jerome Dane <jerome.dane@gmail.com>
2014-01-31 13:45:10 -0500

    #66 Get window class
    As suggested by @remcoros

--- src/net/hearthstats/ProgramHelper.java
+++ src/net/hearthstats/ProgramHelper.java
@@ -76,8 +76,14 @@ public class ProgramHelper extends Observable {
 		      	    User32DLL.GetWindowThreadProcessId(hWnd, pointer);
 		      	    Pointer process = Kernel32.OpenProcess(Kernel32.PROCESS_QUERY_INFORMATION | Kernel32.PROCESS_VM_READ, false, pointer.getValue());
 		      	    Psapi.GetModuleBaseNameW(process, null, buffer, MAX_TITLE_LENGTH);
+		      	    
 		      	    if(Native.toString(buffer).equals(_processName)) {
 		      	    	_windowHandle = hWnd;
+		      	    	
+		      	    	char[] className = new char[512];
+		      	    	User32.INSTANCE.GetClassName(_windowHandle, className, 512);
+		      	    	String classNameStr = Native.toString(className);
+		      	    	
 		      	    	if(_windowHandleId == null) {
 		      	    		_windowHandleId = _windowHandle.toString();
 		      	    		_notifyObserversOfChangeTo(_programName + " window found with process name " + _processName);
