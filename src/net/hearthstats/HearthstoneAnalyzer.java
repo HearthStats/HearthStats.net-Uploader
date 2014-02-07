e5c37ece1f1b77867f0336e01c357ff829ccb3a1
Jerome Dane <jerome.dane@gmail.com>
2014-02-07 09:36:23 -0500

    Fixed silent crash relating to null returned by OCR

--- src/net/hearthstats/HearthstoneAnalyzer.java
+++ src/net/hearthstats/HearthstoneAnalyzer.java
@@ -519,7 +519,7 @@ public class HearthstoneAnalyzer extends Observable {
 		
 		try {
 			String ocrString = OCR.process(newImage);
-			return ocrString.replaceAll("\\s+","");
+			return ocrString == null ? "" : ocrString.replaceAll("\\s+","");
 		} catch(Exception e) {
 			e.printStackTrace();
 			_notifyObserversOfChangeTo("Exception tryint to analyze opponent name image:\n" + e.getMessage());
