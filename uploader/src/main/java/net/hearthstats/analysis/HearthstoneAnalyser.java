3744f447cc35c41967789ec6728ed249b8bda120
Charles Gutjahr <github@charlesgutjahr.com>
2014-04-25 17:15:25 +1000

    Switched logging back to the default INFO level

--- uploader/src/main/java/net/hearthstats/analysis/HearthstoneAnalyser.java
+++ uploader/src/main/java/net/hearthstats/analysis/HearthstoneAnalyser.java
@@ -16,10 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Date;
 import java.util.Observable;
 
 /**
