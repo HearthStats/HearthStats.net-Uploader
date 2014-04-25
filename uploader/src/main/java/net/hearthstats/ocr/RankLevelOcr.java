0802370b7d8ca5f4fabbe4ad7831bc93a39c675c
gtch <com.github@charlesgutjahr.com>
2014-04-25 18:42:19 +1000

    Much more accurate rank detection
    Changed the way the screenshot of the rank is filtered so that only the number is extracted and all background is completely removed. This results in an almost perfect image for OCR, so the rank detection is extremely accurate now.
    
    As a bonus the new filtering method is way faster than the old one, at least ten times faster!

--- uploader/src/main/java/net/hearthstats/ocr/RankLevelOcr.java
+++ uploader/src/main/java/net/hearthstats/ocr/RankLevelOcr.java
@@ -3,6 +3,7 @@ package net.hearthstats.ocr;
 import net.hearthstats.analysis.HearthstoneAnalyser;
 import org.apache.commons.lang3.StringUtils;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class RankLevelOcr extends OcrBase {
@@ -36,8 +37,8 @@ public class RankLevelOcr extends OcrBase {
         int xOffset = HearthstoneAnalyser.getXOffset(image, ratio);
 
         int retryOffset = (iteration - 1) % 3;
-        int x = (int) ((875 + retryOffset) * ratio + xOffset);
-        int y = (int) (162 * ratio);
+        int x = (int) ((877 + retryOffset) * ratio + xOffset);
+        int y = (int) (161 * ratio);
         int width = (int) (32 * ratio);
         int height = (int) (22 * ratio);
 
@@ -79,6 +80,46 @@ public class RankLevelOcr extends OcrBase {
         }
     }
 
+
+    @Override
+    protected BufferedImage filter(BufferedImage image, int iteration) throws OcrException {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int bigWidth = width * 3;
+        int bigHeight = height * 3;
+
+        // Extract only the black & white parts of the image, removing any coloured parts. This results in just the
+        // rank number being left... all the backgroun magically disappears.
+        // Note that this change is being done directly on the source image for efficiency, which is OK because the
+        // source image is thrown out. However if the caller needs to reuse the source image then the following code
+        // should be changed to work on a copy of the image instead of the original.
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int rgba = image.getRGB(x, y);
+                int red = (rgba >> 16) & 0xFF;
+                int green = (rgba >> 8) & 0xFF;
+                int blue = (rgba >> 0) & 0xFF;
+
+                int invertedGatedLevel;
+                if (Math.abs(red - green) > 3 || Math.abs(red - blue) > 3 || Math.abs(green - blue) > 3) {
+                    invertedGatedLevel = 255;
+                } else {
+                    invertedGatedLevel = 255 - blue;
+                }
+
+                Color col = new Color(invertedGatedLevel, invertedGatedLevel, invertedGatedLevel);
+                image.setRGB(x, y, col.getRGB());
+            }
+        }
+
+        // blow it up for ocr
+        BufferedImage newImage = new BufferedImage(bigWidth, bigHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics g = newImage.createGraphics();
+        g.drawImage(image, 0, 0, bigWidth, bigHeight, null);
+        g.dispose();
+
+        return newImage;
+    }
 
     @Override
     protected String getFilename() {
