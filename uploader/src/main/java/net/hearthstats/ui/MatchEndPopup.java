cce04acb82d6dc17cf803e2b73e96a9d783340ba
Michel Daviot <michel@daviot.info>
2014-05-08 15:51:30 +0200

    finish dynamic popup with deck zone

--- uploader/src/main/java/net/hearthstats/ui/MatchEndPopup.java
+++ uploader/src/main/java/net/hearthstats/ui/MatchEndPopup.java
@@ -5,6 +5,7 @@ import net.hearthstats.HearthstoneMatch;
 import net.hearthstats.Monitor;
 import net.hearthstats.util.Rank;
 import net.miginfocom.swing.MigLayout;
+
 import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -56,8 +58,9 @@ public class MatchEndPopup extends JPanel {
     private JRadioButton resultDefeat;
     private JRadioButton resultDraw;
     private JPanel rankPanel=new JPanel();
-
+    private JPanel deckPanel=new JPanel();
 	private JLabel rankNotApplicable;
+	private JLabel deckNotApplicable;
 
 
     private MatchEndPopup(HearthstoneMatch match, String infoMessage) {
@@ -181,8 +184,7 @@ public class MatchEndPopup extends JPanel {
         add(new JLabel(t("match.label.game_mode")), "right");
 
         gameModeComboBox = new JComboBox<>(gameModes);
-        gameModeComboBox.setMinimumSize(new Dimension(180, 27));
-        gameModeComboBox.setPreferredSize(new Dimension(200, 28));
+        setDefaultSize(gameModeComboBox);
         if (match.getMode() != null) {
              gameModeComboBox.setSelectedItem(match.getMode());
         } else {
@@ -192,7 +194,7 @@ public class MatchEndPopup extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
             	match.setMode(gameModeComboBox.getSelectedItem().toString());
-            	updateRankPanel();
+            	updateGameMode();
             }
         });
         add(gameModeComboBox, "span");
@@ -202,8 +204,7 @@ public class MatchEndPopup extends JPanel {
 
         add(new JLabel(t("match.label.your_rank")), "right");
         rankComboBox = new JComboBox<>(Rank.values());
-        rankComboBox.setMinimumSize(new Dimension(180, 27));
-        rankComboBox.setPreferredSize(new Dimension(200, 28));
+        setDefaultSize(rankComboBox);
         rankComboBox.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
@@ -214,15 +215,13 @@ public class MatchEndPopup extends JPanel {
         rankNotApplicable = new JLabel("");
         rankNotApplicable.setFont(rankNotApplicable.getFont().deriveFont(Font.ITALIC));
         rankNotApplicable.setEnabled(false);
+        setDefaultSize(rankNotApplicable);
 
         add(rankPanel, "");
-        updateRankPanel();
-
         add(new JLabel(t("match.label.opponent_name")), "right");
 
         opponentNameField = new JTextField();
-        opponentNameField.setMinimumSize(new Dimension(180, 27));
-        opponentNameField.setPreferredSize(new Dimension(200, 28));
+        setDefaultSize(opponentNameField);
         opponentNameField.setText(match.getOpponentName());
         opponentNameField.addKeyListener(new KeyAdapter() {
         	@Override
@@ -244,8 +243,7 @@ public class MatchEndPopup extends JPanel {
         add(new JLabel(t("match.label.your_class")), "right");
 
         yourClassComboBox = new JComboBox<>(localizedClassOptions);
-        yourClassComboBox.setMinimumSize(new Dimension(180, 27));
-        yourClassComboBox.setPreferredSize(new Dimension(200, 28));
+        setDefaultSize(yourClassComboBox);
         if (match.getUserClass() == null) {
             yourClassComboBox.setSelectedIndex(0);
         } else {
@@ -266,8 +264,7 @@ public class MatchEndPopup extends JPanel {
         add(new JLabel(t("match.label.opponents_class")), "right");
 
         opponentClassComboBox = new JComboBox<>(localizedClassOptions);
-        opponentClassComboBox.setMinimumSize(new Dimension(180, 27));
-        opponentClassComboBox.setPreferredSize(new Dimension(200, 28));
+        setDefaultSize(opponentClassComboBox);
         if (match.getOpponentClass() == null) {
             opponentClassComboBox.setSelectedIndex(0);
         } else {
@@ -291,48 +288,38 @@ public class MatchEndPopup extends JPanel {
 
         add(new JLabel(t("match.label.your_deck")), "right");
 
-        if ("Ranked".equals(match.getMode()) || "Casual".equals(match.getMode())) {
-
-            String[] deckSlotList = new String[10];
-            deckSlotList[0] = undetectedLabel();
-            for (int i = 1; i <= 9; i++) {
-                JSONObject deck =  DeckSlotUtils.getDeckFromSlot(i);
-                StringBuilder sb = new StringBuilder();
-                sb.append(t("deck_slot.label_" + i));
-                sb.append(" ");
-                if (deck == null) {
-                    sb.append(t("undetected"));
-                } else {
-                    sb.append(deck.get("name"));
-                }
-                deckSlotList[i] = sb.toString();
-            }
-
-            yourDeckComboBox = new JComboBox<>(deckSlotList);
-            yourDeckComboBox.setMinimumSize(new Dimension(180, 27));
-            yourDeckComboBox.setPreferredSize(new Dimension(200, 28));
-            yourDeckComboBox.setSelectedIndex(match.getDeckSlot());
-            yourDeckComboBox.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    match.setDeckSlot(yourDeckComboBox.getSelectedIndex());
-                }
-            });
-            add(yourDeckComboBox, "wrap");
-
-        } else {
-            String deckMessage;
-            if ("Arena".equals(match.getMode())) {
-                deckMessage = "N/A: Arena Mode";
+        String[] deckSlotList = new String[10];
+        deckSlotList[0] = undetectedLabel();
+        for (int i = 1; i <= 9; i++) {
+            JSONObject deck =  DeckSlotUtils.getDeckFromSlot(i);
+            StringBuilder sb = new StringBuilder();
+            sb.append(t("deck_slot.label_" + i));
+            sb.append(" ");
+            if (deck == null) {
+                sb.append(t("undetected"));
             } else {
-                deckMessage = "N/A";
+                sb.append(deck.get("name"));
             }
-            JLabel rankNotApplicable = new JLabel(deckMessage);
-            rankNotApplicable.setFont(rankNotApplicable.getFont().deriveFont(Font.ITALIC));
-            rankNotApplicable.setEnabled(false);
-            add(rankNotApplicable, "wrap");
+            deckSlotList[i] = sb.toString();
         }
 
+        yourDeckComboBox = new JComboBox<>(deckSlotList);
+        setDefaultSize(yourDeckComboBox);
+        yourDeckComboBox.setSelectedIndex(match.getDeckSlot());
+        yourDeckComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                match.setDeckSlot(yourDeckComboBox.getSelectedIndex());
+            }
+        });
+        
+      
+        deckNotApplicable = new JLabel("");
+        deckNotApplicable.setFont(deckNotApplicable.getFont().deriveFont(Font.ITALIC));
+        deckNotApplicable.setEnabled(false);
+        setDefaultSize(deckNotApplicable);
+        
+        add(deckPanel, "wrap");
 
         //// Row 7 ////
 
@@ -429,9 +416,10 @@ public class MatchEndPopup extends JPanel {
         });
         add(notesTextArea, "span 3, wrap");
 
+        updateGameMode();
     }
     
-	private void updateRankPanel() {
+	private void updateGameMode() {
 		boolean isRanked = "Ranked".equals(match.getMode());
 		rankPanel.removeAll();
 		if (isRanked) {
@@ -451,9 +439,29 @@ public class MatchEndPopup extends JPanel {
 			rankNotApplicable.setText(rankMessage);
 			rankPanel.add(rankNotApplicable);
 		}
+
+		deckPanel.removeAll();
+		if (isRanked || "Casual".equals(match.getMode())) { // TODO shouldn't we add also Practice here ?
+			deckPanel.add(yourDeckComboBox);
+		} else {
+			String deckMessage;
+			if ("Arena".equals(match.getMode())) {
+				deckMessage = "N/A: Arena Mode";
+			} else {
+				deckMessage = "N/A";
+			}
+			deckNotApplicable.setText(deckMessage);
+			deckPanel.add(deckNotApplicable);
+		}
+
 		validate();
+		repaint();
 	}
 
+	private static void setDefaultSize(Component c) {
+		c.setMinimumSize(new Dimension(180, 27));
+		c.setPreferredSize(new Dimension(200, 28));
+	}
 
     /**
      * Loads text from the main resource bundle, using the local language when available.
