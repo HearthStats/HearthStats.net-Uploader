2349ea122ed5599fdeaaac68c23064b7221cea26
Michel Daviot <michel@daviot.info>
2014-06-07 13:48:37 +0200

    test to check for message "card not found in deck"
    This message was displayed even when the previous deck overlay was
    closed

--- uploader/src/test/scala/net/hearthstats/logmonitor/LogMonitorDeckOverlayFor2DecksMain.scala
+++ uploader/src/test/scala/net/hearthstats/logmonitor/LogMonitorDeckOverlayFor2DecksMain.scala
@@ -9,22 +9,26 @@ import net.hearthstats.ui.ClickableDeckBox
 import javax.swing.JFrame
 import javax.swing.JDialog
 
-object LogMonitorDeckOverlayMain extends App {
+object LogMonitorDeckOverlayFor2DecksMain extends App {
   val tempLogFile = File.createTempFile("hssample", "log")
   println(s"monitorin $tempLogFile ")
   val monitor = new HearthstoneLogMonitor(tempLogFile.getAbsolutePath)
 
-  val deck = DeckUtils.getDeck(20034)
-  ClickableDeckBox.showBox(deck, monitor.cardEvents)
+  val deck1 = DeckUtils.getDeck(20034)
+  val deck2 = DeckUtils.getDeck(20462)
+
+  val box1 = ClickableDeckBox.showBox(deck1, monitor.cardEvents)
+
   new Thread {
     override def run() = {
       val writer = new BufferedWriter(new FileWriter(tempLogFile))
       Thread.sleep(500)
       writer.write(initialHand)
       writer.flush()
-      Thread.sleep(5000)
-      //      ClickableDeckBox.showBox(deck, monitor.cardEvents)
-      writer.write(mulligan)
+      Thread.sleep(10000)
+      ClickableDeckBox.showBox(deck2, monitor.cardEvents)
+      writer.write(initialHand2)
+      writer.flush()
       writer.close()
     }
   }.start()
@@ -36,13 +40,8 @@ object LogMonitorDeckOverlayMain extends App {
 [Zone] ZoneChangeList.ProcessChanges() - id=2 local=False [name=Twilight Drake id=13 zone=HAND zonePos=0 cardId=EX1_043 player=1] zone from FRIENDLY DECK -> FRIENDLY HAND
     """
 
-  def mulligan =
+  def initialHand2 =
     """
-[Zone] ZoneChangeList.ProcessChanges() - id=4 local=False [name=Soulfire id=17 zone=DECK zonePos=1 cardId=EX1_308 player=1] zone from FRIENDLY HAND -> FRIENDLY DECK
-[Zone] ZoneChangeList.ProcessChanges() - id=4 local=False [name=Sunfury Protector id=31 zone=DECK zonePos=2 cardId=EX1_058 player=1] zone from FRIENDLY HAND -> FRIENDLY DECK
-[Zone] ZoneChangeList.ProcessChanges() - id=4 local=False [name=Twilight Drake id=13 zone=DECK zonePos=3 cardId=EX1_043 player=1] zone from FRIENDLY HAND -> FRIENDLY DECK
-
-[Zone] ZoneChangeList.ProcessChanges() - id=4 local=False [name=Ancient Watcher id=20 zone=HAND zonePos=0 cardId=EX1_045 player=1] zone from FRIENDLY DECK -> FRIENDLY HAND
-[Zone] ZoneChangeList.ProcessChanges() - id=4 local=False [name=Twilight Drake id=13 zone=HAND zonePos=3 cardId=EX1_043 player=1] zone from FRIENDLY DECK -> FRIENDLY HAND
+[Zone] ZoneChangeList.ProcessChanges() - id=2 local=False [name=Animal Companion id=17 zone=HAND zonePos=0 cardId=EX1_308 player=1] zone from FRIENDLY DECK -> FRIENDLY HAND
     """
 }
