313f42625b79516427ff0d2ca0d5386ef029c173
Charles Gutjahr <github@charlesgutjahr.com>
2014-06-11 21:58:16 +1000

    Reformatted to Google Java Style guide
    No code changes, this change just gets the formatting consistent by using 2-space indentation instead of tabs or 4-space.

--- uploader-osx/src/main/java/net/hearthstats/osx/jna/NSBitmapImageRep.java
+++ uploader-osx/src/main/java/net/hearthstats/osx/jna/NSBitmapImageRep.java
@@ -5,34 +5,36 @@ import org.rococoa.ID;
 import org.rococoa.NSClass;
 import org.rococoa.Rococoa;
 
+
 /**
  * @author gtch
  */
 public interface NSBitmapImageRep extends NSImageRep {
 
-	public static final _Class CLASS = Rococoa.createClass("NSBitmapImageRep",
-			_Class.class);
+  public static final _Class CLASS = Rococoa.createClass("NSBitmapImageRep",
+    _Class.class);
 
-	public interface _Class extends NSClass {
-		NSBitmapImageRep alloc();
-	}
 
-	public abstract NSBitmapImageRep initWithCGImage(ID cgImage);
-	public abstract Pointer bitmapData();
-	public abstract int samplesPerPixel();
-	public abstract int bitsPerPixel();
-	public abstract int bytesPerRow();
-	public abstract int bytesPerPlane();
-	public abstract int numberOfPlanes();
-	public abstract int bitmapFormat();
+  public interface _Class extends NSClass {
+    NSBitmapImageRep alloc();
+  }
 
-	public static interface NSBitmapFormat {
-		// / 0 means is alpha last (RGBA, CMYKA, etc.)
-		public static final int NSAlphaFirstBitmapFormat = 1 << 0;
-		// / 0 means is premultiplied
-		public static final int NSAlphaNonpremultipliedBitmapFormat = 1 << 1;
-		// / 0 is integer
-		public static final int NSFloatingPointSamplesBitmapFormat = 1 << 2;
-	}
+  public abstract NSBitmapImageRep initWithCGImage(ID cgImage);
+  public abstract Pointer bitmapData();
+  public abstract int samplesPerPixel();
+  public abstract int bitsPerPixel();
+  public abstract int bytesPerRow();
+  public abstract int bytesPerPlane();
+  public abstract int numberOfPlanes();
+  public abstract int bitmapFormat();
+
+  public static interface NSBitmapFormat {
+    // / 0 means is alpha last (RGBA, CMYKA, etc.)
+    public static final int NSAlphaFirstBitmapFormat = 1 << 0;
+    // / 0 means is premultiplied
+    public static final int NSAlphaNonpremultipliedBitmapFormat = 1 << 1;
+    // / 0 is integer
+    public static final int NSFloatingPointSamplesBitmapFormat = 1 << 2;
+  }
 
 }
