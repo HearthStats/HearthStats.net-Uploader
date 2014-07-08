9f40375e2d370909bb9dbd4e5f68008cf2f6c973
Michel Daviot <michel@daviot.info>
2014-07-09 00:11:00 +0200

    minor refactoring

--- uploader/src/main/java/net/hearthstats/ui/MatchEndPopup.java
+++ uploader/src/main/java/net/hearthstats/ui/MatchEndPopup.java
@@ -37,8 +37,6 @@ import net.hearthstats.util.Rank;
 import net.miginfocom.swing.MigLayout;
 
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A popup to display at the end of the match that allows the match details to
@@ -48,7 +46,6 @@ import org.slf4j.LoggerFactory;
  */
 public class MatchEndPopup extends JPanel {
 
-  private final static Logger debugLog = LoggerFactory.getLogger(MatchEndPopup.class);
 
   private final ResourceBundle bundle = ResourceBundle.getBundle("net.hearthstats.resources.Main");
 
@@ -91,20 +88,14 @@ public class MatchEndPopup extends JPanel {
         JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION, null, new String[] { "Submit",
             "Cancel" }, "Submit");
 
-    Button result;
     switch (value) {
     case 0:
-      result = Button.SUBMIT;
-      break;
+      return Button.SUBMIT;
     case 1:
-      result = Button.CANCEL;
-      break;
+      return Button.CANCEL;
     default:
-      result = Button.CANCEL;
-      break;
+      return Button.CANCEL;
     }
-
-    return result;
   }
 
   private List<String> determineErrors(HearthstoneMatch match) {
