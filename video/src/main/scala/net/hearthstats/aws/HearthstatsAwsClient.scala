8f4e2112b0e4ab153471ce414332e531b8d44cb4
Michel Daviot <michel@daviot.info>
2014-07-14 21:30:21 +0200

    AWS S3 client code updated

---
+++ video/src/main/scala/net/hearthstats/aws/HearthstatsAwsClient.scala
@@ -0,0 +1,43 @@
+package net.hearthstats.aws
+
+import java.io.File
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.event.{ProgressEvent, ProgressListener}
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.{ObjectMetadata, PutObjectRequest, PutObjectResult}
+
+import grizzled.slf4j.Logging
+
+class HearthstatsAwsClient(accessKey: String, secretKey: String) extends Logging {
+  val s3 = new AmazonS3Client(new AWSCredentials {
+    val getAWSAccessKeyId = accessKey
+    val getAWSSecretKey = secretKey
+  })
+
+  def storeFile(bucket: String, prefix: String, file: File): Future[PutObjectResult] = Future {
+    val name = file.getName
+    info(s"Uploading a new object to S3 from $name to $prefix")
+    val metadata = new ObjectMetadata
+    metadata.setContentLength(file.length)
+    val request = new PutObjectRequest(bucket, prefix + name, file)
+    request.setMetadata(metadata)
+    request.setGeneralProgressListener(new LogProgressListener)
+    s3.putObject(request)
+  }
+
+  class LogProgressListener extends ProgressListener with Logging {
+    var total = 0L
+    def progressChanged(progressEvent: ProgressEvent): Unit = {
+      val size = progressEvent.getBytesTransferred
+      total += size
+      debug(total / 1024 + " kb transfered ")
+      if (progressEvent.getEventCode == ProgressEvent.COMPLETED_EVENT_CODE) {
+        info(" transfer completed  ******")
+      }
+    }
+  }
+}
--- video/src/test/scala/net/hearthstats/aws/SampleAwsClient.scala
+++ video/src/test/scala/net/hearthstats/aws/SampleAwsClient.scala
@@ -1,67 +1,32 @@
 package net.hearthstats.aws
 
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.io.Writer
-import java.util.UUID
-import com.amazonaws.AmazonClientException
-import com.amazonaws.AmazonServiceException
-import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider
-import com.amazonaws.regions.Region
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.Bucket
-import com.amazonaws.services.s3.model.GetObjectRequest
+import java.io.{ File, FileOutputStream, OutputStreamWriter }
+
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
+
 import com.amazonaws.services.s3.model.ListObjectsRequest
-import com.amazonaws.services.s3.model.ObjectListing
-import com.amazonaws.services.s3.model.PutObjectRequest
-import com.amazonaws.services.s3.model.S3Object
-import com.amazonaws.services.s3.model.S3ObjectSummary
-import scala.collection.JavaConversions._
-import com.amazonaws.auth.AWSCredentials
-import com.amazonaws.services.s3.model.ListBucketsRequest
 
 object S3Sample extends App {
-  val s3 = new AmazonS3Client(new AWSCredentials {
-    val getAWSAccessKeyId = "key"
-    val getAWSSecretKey = "secret"
-  })
-  val usWest2 = Region.getRegion(Regions.US_WEST_2)
-  s3.setRegion(usWest2)
+  val client = new HearthstatsAwsClient(args(0), args(1))
+  val s3 = client.s3
   println("Listing buckets")
-  for (bucket <- s3.listBuckets(new ListBucketsRequest)) {
+
+  for (bucket <- s3.listBuckets) {
     println(" - " + bucket.getName)
   }
-  sys.exit()
-  val bucketName = "my-first-s3-bucket-" + UUID.randomUUID()
-  val key = "MyObjectKey"
-  println("Creating bucket " + bucketName + "\n")
-  s3.createBucket(bucketName)
-  println()
-  println("Uploading a new object to S3 from a file\n")
-  s3.putObject(new PutObjectRequest(bucketName, key, createSampleFile()))
-  println("Downloading an object")
-  val `object` = s3.getObject(new GetObjectRequest(bucketName, key))
-  println("Content-Type: " + `object`.getObjectMetadata.getContentType)
-  displayTextInputStream(`object`.getObjectContent)
-  println("Listing objects")
-  val objectListing = s3.listObjects(new ListObjectsRequest().withBucketName(bucketName)
-    .withPrefix("My"))
-  for (objectSummary <- objectListing.getObjectSummaries) {
-    println(" - " + objectSummary.getKey + "  " + "(size = " + objectSummary.getSize +
-      ")")
+  val bucketName = "hearthstats-dev"
+
+  //  client.storeFile(bucketName, "video/DEMO/", new File("""C:\Users\tyrcho\AppData\Local\Temp\video3451076409001028894.mp4""")).onSuccess {
+  //    case res => println(s"Success : $res")
+  //  }
+  client.storeFile(bucketName, "video/REMOVE", createSampleFile()).onSuccess {
+    case res => println(s"Success : ${res.getContentMd5}")
   }
-  println()
-  println("Deleting an object\n")
-  s3.deleteObject(bucketName, key)
-  println("Deleting bucket " + bucketName + "\n")
-  s3.deleteBucket(bucketName)
+
+  for (objectSummary <- s3.listObjects(new ListObjectsRequest().withBucketName(bucketName)).getObjectSummaries) {
+    println(s" - ${objectSummary.getKey} (size = ${objectSummary.getSize})")
+  }
 
   /**
    * Creates a temporary file with text data to demonstrate uploading a file
@@ -82,23 +47,5 @@ object S3Sample extends App {
     writer.write("abcdefghijklmnopqrstuvwxyz\n")
     writer.close()
     file
-  }
-
-  /**
-   * Displays the contents of the specified input stream as text.
-   *
-   * @param input
-   *            The input stream to display as text.
-   *
-   * @throws IOException
-   */
-  private def displayTextInputStream(input: InputStream) {
-    val reader = new BufferedReader(new InputStreamReader(input))
-    while (true) {
-      val line = reader.readLine()
-      if (line == null) //break
-        println("    " + line)
-    }
-    println()
   }
 }
