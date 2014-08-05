204397727b4e950c676bc572b05afb4771f09b6e
Shyos <Shyos>
2014-08-05 19:10:58 +1000

    Merged the Deck Export Feature from master to v0.19 branch (cherry picked from commit 5acb61ed32e08cf42ce6645cab759b03578733f1)

---
+++ uploader/src/main/java/zulu/deckexport/extracter/Constants.java
@@ -0,0 +1,11 @@
+/**
+ * @author shyos
+ */
+package zulu.deckexport.extracter;
+
+public class Constants {
+	public static int probItemLimit = 5; // # of best possible cards for each card (Can be increased for better detection)
+	public static String txtCards = "/zulu/deckexport/txt/cards.txt";
+	public static String txtCardCounts = "/zulu/deckexport/txt/cardcounts.txt";
+	
+}
