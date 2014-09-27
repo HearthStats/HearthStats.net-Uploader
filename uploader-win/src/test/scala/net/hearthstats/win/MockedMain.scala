8049b82822385e566ca2c4751bb55e42b48aca21
Michel Daviot <michel@daviot.info>
2014-09-27 16:35:21 +0200

    chain 2 games in integration test

--- uploader-win/src/test/scala/net/hearthstats/win/MockedMain.scala
+++ uploader-win/src/test/scala/net/hearthstats/win/MockedMain.scala
@@ -15,14 +15,19 @@ import net.hearthstats.game.imageanalysis.HsClassAnalyser
 object MockedMain extends TesseractSetup with App with Logging {
   val environment = TestEnvironment
   val config = TestConfig
-  val helper = new MockProgramHelper(List(
-    "play_lobby" -> 1,
-    "finding" -> 10,
-    "Druid_VS_Hunter" -> 1,
-    "starting_hand_4_cards" -> 1,
-    "orgrimmar_with_coin" -> 1,
-    //    "victory_pandaria" -> 1))
-    "defeat_stranglehorn" -> 1))
+
+  def game(players: String, result: String) =
+    List(
+      "play_lobby" -> 1,
+      "finding" -> 10,
+      players -> 1,
+      "starting_hand_4_cards" -> 1,
+      "orgrimmar_with_coin" -> 1,
+      result -> 15)
+
+  val firstGame = game("Druid_VS_Hunter", "defeat_stranglehorn")
+  val secondGame = game("Priest_VS_Warlock", "victory_pandaria")
+  val helper = new MockProgramHelper(firstGame ++ secondGame)
   val main = wire[Main]
 
   setupTesseract()
