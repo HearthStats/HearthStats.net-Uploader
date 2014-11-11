170aa13779af61e526f85d4ab7ab993da70a87b5
Michel Daviot <michel@daviot.info>
2014-11-11 14:46:37 +0100

    restored main class for tests

---
+++ companion/src/test/scala/net/hearthstats/game/LogParserMain.scala
@@ -0,0 +1,18 @@
+package net.hearthstats.game
+
+import scala.concurrent.duration.DurationInt
+import scala.collection.JavaConversions._
+
+object LogParserMain extends App {
+  val p = new LogParser
+  val lines = io.Source.fromInputStream(getClass.getResourceAsStream("several_games_log.txt")).getLines
+
+  for {
+    l <- lines
+    r <- p.analyseLine(l)
+  } {
+    println(r)
+    if (r.isInstanceOf[GameOver]) System.exit(0)
+  }
+}
+
